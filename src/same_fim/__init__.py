03589150d80277d53d273df53edc2842e941d5a3
Massyl Benarab <massylbenarab28@gmail.com>
2026-04-20 12:59:11 +0100

    Bump to 0.7.3 (0.7.2 uploaded before maintainer fix)

--- src/same_fim/__init__.py
+++ src/same_fim/__init__.py
@@ -26,7 +26,7 @@ from .core import (
 
 Rule = AssociationRule  # public alias
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 __all__ = [
     "SAME", "SAMEResult", "Itemset", "AssociationRule", "Rule",
